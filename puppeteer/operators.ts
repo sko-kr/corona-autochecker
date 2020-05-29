594722e4ede7adce67263edb1c326dce6e5b902e
Seunghoon Ko <ko@toss.im>
2020-05-29 13:56:45 +0900

    define input, click, and wait for navigation epic helpers

---
+++ puppeteer/operators.ts
@@ -0,0 +1,42 @@
+import { switchMap } from 'rxjs/operators';
+import { CommonBrowser } from '../models/CommonBrowser';
+import { randomHumanDelay } from './utils';
+
+export function input(selector: string, text = '', options = {
+  maxTypingDelay: 60,
+  minTypingDelay: 20
+}) {
+  return switchMap(async ({ page, browser }: CommonBrowser) => {
+    if (text === '') {
+      console.warn(`${selector} 의 값을 써주세요.`);
+    }
+    const randomTypeDelay = randomHumanDelay({ maxMs: options.maxTypingDelay, minMs: options.minTypingDelay });
+    await page.type(selector, text, { delay: randomTypeDelay })
+    return { page, browser }
+  })
+}
+
+export function click(selector: string) {
+  return switchMap(async ({ page, browser }: CommonBrowser) => {
+    await page.click(selector)
+    return { page, browser };
+  })
+}
+
+export function clickComplete(selector: string, options = { navigationTimeout: 60000 }) {
+  return switchMap(async ({ page, browser }: CommonBrowser) => {
+    const nextPageLoaded = new Promise((resolve, reject) => {
+      const timeoutId = setTimeout(() => reject(), options.navigationTimeout)
+      page.once('load', () => {
+        clearTimeout(timeoutId);
+        resolve()
+      })
+    })
+    await Promise.all([
+      page.click(selector),
+      nextPageLoaded
+    ]);
+    return { page, browser };
+  })
+}
+
