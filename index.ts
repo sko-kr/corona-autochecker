0297639f551c57f502dba617aed67a482e49ebbb
Seunghoon Ko <ko@toss.im>
2020-05-30 15:33:03 +0900

    restart on error

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import FileSync from 'lowdb/adapters/FileSync'
 import low from 'lowdb';
 import { from, of, timer } from 'rxjs';
-import { filter, mergeMap, switchMap, tap } from 'rxjs/operators';
+import { delay, filter, mergeMap, retryWhen, switchMap, take, tap } from 'rxjs/operators';
 import { isNotAlreadyChecked, isValidCheckTime, updateCheckedDate } from './utils/utils';
 import { User } from './models/Database';
 import { coronaCheckEpic } from './epics/coronaCheckEpic';
@@ -26,9 +26,13 @@ timer(0, TEN_MINUTES).pipe(
   tap(({ user }) => updateCheckedDate(db, user)),
   tap(({ browser, user }: { browser: Browser, user: User }) => {
     browser.close()
+  }),
+  retryWhen(error$ => {
+    return error$.pipe(delay(TEN_MINUTES * 3), take(100))
   })
 ).subscribe(() => {
   /**TODO: notify successful check*/
 }, () => {
   /**TODO: notify failed check*/
+  throw Error(`Let PM2 restart`)
 })
