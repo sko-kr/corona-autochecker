69187a870321d9d9cd239df686c7e67148b346b1
Seunghoon Ko <ko@toss.im>
2020-05-31 11:35:22 +0900

    refactor launch page to return observable rather than operator function

--- puppeteer/launch.ts
+++ puppeteer/launch.ts
@@ -1,4 +1,6 @@
 import { switchMap } from 'rxjs/operators';
+import { from } from 'rxjs';
+import { Browser } from 'puppeteer';
 
 const puppeteer = require('puppeteer');
 
@@ -7,9 +9,12 @@ interface Params {
   devMode?: boolean;
 }
 
-export const launchPage = ({ url, devMode }: Params) => switchMap(async () => {
-  const browser = await puppeteer.launch({ headless: !devMode });
-  const page = await browser.newPage();
+const launchBrowser = ({ devMode }: { devMode?: boolean }) => from<Promise<Browser>>(puppeteer.launch({ headless: !devMode }))
+
+const openPage = ({ url }: { url: string }) => switchMap(async (browser: Browser) => {
+  const page = await browser.newPage()
   await page.goto(url, { waitUntil: 'networkidle2' });
   return { browser, page };
 })
+
+export const launchPage = ({ url, devMode }: Params) => launchBrowser({ devMode }).pipe(openPage({ url }))
