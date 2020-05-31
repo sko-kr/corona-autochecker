824d91eb90aafc294206f5a4796b2e891ada0b2d
Seunghoon Ko <ko@toss.im>
2020-05-31 11:42:00 +0900

    make index pipe more readable by; * making one line per operation. * point free whenever possible.

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import dotenv from 'dotenv';
 import FileSync from 'lowdb/adapters/FileSync'
 import low from 'lowdb';
-import { from, of, timer } from 'rxjs';
+import { from, timer } from 'rxjs';
 import { delay, filter, mergeMap, retryWhen, switchMap, take, tap } from 'rxjs/operators';
 import { isNotAlreadyChecked, isValidCheckTime, updateCheckedDate } from './utils/utils';
 import { User } from './models/Database';
 import { coronaCheckEpic } from './epics/coronaCheckEpic';
-import { Browser } from 'puppeteer';
 
 const TEN_MINUTES = 1000 * 60 * 10
 
@@ -17,19 +16,12 @@ const db = low(adapter)
 
 timer(0, TEN_MINUTES).pipe(
   filter(isValidCheckTime),
-  switchMap(() => {
-    const users: Array<User> = db.get('users').value();
-    return from(users)
-  }),
+  switchMap(() => from<Array<User>>(db.get('users').value())),
   filter(isNotAlreadyChecked),
-  mergeMap(user => of(1).pipe(coronaCheckEpic(user)), 10),
+  mergeMap(coronaCheckEpic, 10),
   tap(({ user }) => updateCheckedDate(db, user)),
-  tap(({ browser, user }: { browser: Browser, user: User }) => {
-    browser.close()
-  }),
-  retryWhen(error$ => {
-    return error$.pipe(delay(TEN_MINUTES), take(100))
-  })
+  tap(({ browser }) => browser.close()),
+  retryWhen(error$ => error$.pipe(delay(TEN_MINUTES), take(100)))
 ).subscribe(() => {
   /**TODO: notify successful check*/
 }, () => {
