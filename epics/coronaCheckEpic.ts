46a61de02c9467d89abfe690738c824544ba5835
Seunghoon Ko <ko@toss.im>
2020-05-31 11:40:24 +0900

    refactor coronaCheckEpic as callback + group pipe for type reason. * type reason: pipe operator with more than 9 operations lose type. (https://github.com/ReactiveX/rxjs/issues/4177) * coronaCheckEpic receives POJO, ans returns observable hence is a callback passed to operator, rather than an operator itself.

--- epics/coronaCheckEpic.ts
+++ epics/coronaCheckEpic.ts
@@ -1,4 +1,4 @@
-import { Observable, pipe, UnaryFunction } from 'rxjs';
+import { pipe } from 'rxjs';
 import { launchPage } from '../puppeteer/launch';
 import {
   clickPupilInputComplete,
@@ -17,29 +17,32 @@ import { delay, map, tap } from 'rxjs/operators';
 import { Browser, Page } from 'puppeteer';
 import { randomHumanDelay } from '../utils/utils';
 
-export function coronaCheckEpic(user: User): UnaryFunction<Observable<any>, Observable<{ browser: Browser, page: Page, user: User }>>;
 export function coronaCheckEpic(user: User) {
-  return pipe(
-    launchPage({ url: 'https://eduro.sen.go.kr/stv_cvd_co00_002.do', devMode: process.env.NODE_ENV === 'development' }),
-    inputSchoolName(user.schoolName),
-    inputPupilName(user.name),
-    inputPupilDob(user.dob),
-    clickPupilInputComplete(),
-    /** Next page */
-    selectUnder37p5(),
-    selectNoSymptom(),
-    selectNoSelfTraveled(),
-    selectNoRelativeHasTraveled(),
-    selectNoRelativeHasCorona(),
-    clickSurveyComplete(),
-    tap(({page}) => {
-      page.screenshot({path: `${user.name}${new Date().getTime()}`})
-    }),
-    delay(randomHumanDelay({ minMs: 3000, maxMs: 6000 })),
-    map(({ browser, page }: { browser: Browser; page: Page }): { browser: Browser, page: Page, user: User } => ({
-      browser,
-      page,
-      user
-    }))
-  );
+  return launchPage({ url: 'https://eduro.sen.go.kr/stv_cvd_co00_002.do', devMode: process.env.NODE_ENV === 'development' }).pipe(
+    pipe(
+      inputSchoolName(user.schoolName),
+      inputPupilName(user.name),
+      inputPupilDob(user.dob),
+      clickPupilInputComplete(),
+    ),
+    pipe(
+      selectUnder37p5(),
+      selectNoSymptom(),
+      selectNoSelfTraveled(),
+      selectNoRelativeHasTraveled(),
+      selectNoRelativeHasCorona(),
+      clickSurveyComplete(),
+    ),
+    pipe(
+      tap(({ page }) => {
+        page.screenshot({ path: `${user.name}${new Date().getTime()}` })
+      }),
+      delay(randomHumanDelay({ minMs: 3000, maxMs: 6000 })),
+      map(({ browser, page }: { browser: Browser; page: Page }): { browser: Browser, page: Page, user: User } => ({
+        browser,
+        page,
+        user
+      }))
+    )
+  )
 }
