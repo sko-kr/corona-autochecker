eed4ed168cf954890f0cc491873f519ad980b602
Seunghoon Ko <ko@toss.im>
2020-06-01 09:11:28 +0900

    TODO: check existence of event listener for page availability.

--- puppeteer/launch.ts
+++ puppeteer/launch.ts
@@ -1,8 +1,6 @@
 import { switchMap } from 'rxjs/operators';
 import { from } from 'rxjs';
-import { Browser } from 'puppeteer';
-
-const puppeteer = require('puppeteer');
+import puppeteer, { Browser } from 'puppeteer';
 
 interface Params {
   url: string;
@@ -12,17 +10,26 @@ interface Params {
 const launchBrowser = ({ devMode }: { devMode?: boolean }) => from<Promise<Browser>>(puppeteer.launch({ headless: !devMode }))
 
 const openPage = ({ url }: { url: string }) => switchMap(async (browser: Browser) => {
-  const page = await browser.newPage()
+  /**TODO intercept listener to make sure button click event is bound*/
+  (HTMLButtonElement.prototype as any).realAddEventListener = HTMLButtonElement.prototype.addEventListener;
+  HTMLButtonElement.prototype.addEventListener = function(a: any,b: any,c: any){
+    //SHOOT CUSTOM EVENT which will be read by puppeteer;
+    // ...
+    (this as any).realAddEventListener(a,b,c);
+  };
+  const page = await browser.newPage();
   await page.goto(url, { waitUntil: ['load', 'networkidle0'] });
+  await wait(1000);
   return { browser, page };
 })
 
 const reloadPageIfNoInput = switchMap(async ({ browser, page }) => {
-  let pageExists = await page.$('#pName');
+  let hasClickListener = false; /**TODO: */
   let count = 0
-  while (!pageExists && count < 10) {
+  while (!hasClickListener && count < 10) {
     await page.reload({ waitUntil: ['load', 'networkidle0'] })
-    pageExists = await page.$('#pName')
+    await wait(1000);
+    hasClickListener = true /**TODO: */
     ++count;
   }
   return { browser, page };
@@ -32,3 +39,7 @@ export const launchPage = ({ url, devMode }: Params) => launchBrowser({ devMode 
   openPage({ url }),
   reloadPageIfNoInput
 )
+
+function wait(ms: number) {
+  return new Promise(resolve => setTimeout(() => resolve(), ms))
+}
