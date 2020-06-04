2f758f3a0123ac97867f19b542a10b75bd4571ae
Seunghoon Ko <ko@toss.im>
2020-06-05 00:37:28 +0900

    remove no longer used environment variable.

--- models/Environment.d.ts
+++ models/Environment.d.ts
@@ -2,8 +2,5 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     /** just to clarify NODE_ENV is used in application logic */
     NODE_ENV: string;
-    SCHOOL_NAME: string;
-    PUPIL_NAME: string;
-    PUPIL_DOB: string;
   }
 }
